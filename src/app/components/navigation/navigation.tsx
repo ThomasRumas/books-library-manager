a8b2dd8ddb9a97e33485a56dd5c20f5a8a41a0c6
Thomas Rumas <thomas@rumas.fr>
2025-01-20 10:21:03 +0100

    feat(ui): rework ui

--- src/app/components/navigation/navigation.tsx
+++ src/app/components/navigation/navigation.tsx
@@ -2,13 +2,27 @@ import styles from './navigation.module.scss';
 
 export function NavigationComponent() {
     return(
-        <nav className={styles.nav}>
-            <ul>
-                <li><a href="">Home</a></li>
-                <li><a href="">Search</a></li>
-                <li><a href="">Manage</a></li>
-            </ul>
-            <button className={styles.login}>Login</button>
-        </nav>
+        <div className={styles.nav}>
+            <nav className={`container`}>
+                <div className="row">
+                    <div className="col-8">
+                        <ul>
+                            <li><a href="/">Home</a></li>
+                            <li><a href="/search">Search</a></li>
+                        </ul>
+                    </div>
+                    <div className={`col-4 ${styles['txt-right']}`}>
+                        <ul>
+                            <li>
+                                <a href="/login">Login</a>
+                            </li>
+                            <li>
+                                <a href="/login/register">Register</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+        </div>
     )
 }
